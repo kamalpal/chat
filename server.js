833ed25badd0a8422ba101cb3cfd5699e1096681
Kamal Pal <kamal.pal@andamen.com>
2020-07-31 00:07:53 +0530

    - Added express, socket.io, moments  - Added and configured webpack for compiling assets (style-loader, css-loader)  - Added tailwind for managing css  - Added nodemon to automatically rerun on changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'src')));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
