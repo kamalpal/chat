0e669b0d07af6b7c5d809c49ad539da1fc35cf21
Kamal Pal <kamal.pal@andamen.com>
2020-08-01 20:31:17 +0530

    - Added bare minimum chat functionality.  - Added dist directory in .gitignore.

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'src')));
 
+// Run when client connets
+io.on('connection', socket => {
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to chat');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'Hey, A user has joined the chat');
+
+    // When client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'Hey, A user has left the chat');
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
+})
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "../node_modules/tailwindcss/dist/tailwind.css";
+import io from "socket.io-client";
+
+const chatForm = document.getElementById('chatForm');
+const chatContainer = document.getElementById('chat-content');
+const socket = io();
+
+// Message from server
+socket.on('message', msg => {
+    outputMessage(msg);
+
+    // Scroll down
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+});
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // Get message text
+    const msg = e.target.elements.msgbox.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+    // Clear input & focus
+    e.target.elements.msgbox.value = '';
+    e.target.elements.msgbox.focus();
+});
+
+// output message to DOM
+function outputMessage(msg) {
+    const div = document.createElement('div');
+    div.classList.add('msg-block', 'bg-blue-200', 'p-2', 'mb-2');
+    div.innerHTML = `
+        <span class="username text-blue-700 text-sm">Kamal</span>
+        <span class="time text-gray-600 text-sm">2020-10-01 12:56:32</span>
+        <div class="msg">${msg}</div>
+    `;
+    document.getElementById('chat-content').appendChild(div);
+}
